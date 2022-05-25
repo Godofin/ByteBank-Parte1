ccdaa17ecb210e522b7ef82869fd60a2d65e7817
Guilherme Lavezzo <guilherm909088@gmail.com>
2022-05-25 15:58:16 -0300

    Salvando challenge 3

---
+++ Challenge03/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿namespace hospital
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public double altura;
+        public double peso;
+
+    }
+}
---
+++ Challenge03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Já sabemos o que são tipo de valor, referência e valor padrão de campos no .NET. 
+//Sendo assim, imagine que você precisa cadastrar uma pessoa num sistema médico. 
+//Para isso, será necessário criar uma classe Pessoa com os campos referentes ao nome, idade, altura, peso 
+//de cada paciente e por fim exibir no Program.cs o valor padrão de cada um dos campos.
+
+using hospital;
+
+Pessoa pessoa1 = new Pessoa();
+
+Console.WriteLine($"O nome do paciente é: {pessoa1.nome}\n" +
+    $"A idade do paciente é de: {pessoa1.idade} anos\n" +
+    $"A altura da pessoa é de: {pessoa1.altura} metros\n" +
+    $"O peso da pessoa é de: {pessoa1.peso} Kg");
